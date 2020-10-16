1afa768f0851435907d8cf375f5e1e1c83654d59
Carlos Andrés Álvarez Restrepo <charlie_cha@outlook.com>
2020-10-15 19:49:43 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
+
+# Enable all 8 cores
+CORES=(
+	# 0
+	# 1
+	# 2
+	# 3
+	4
+	5
+	6
+	7 
+)
+for i in ${CORES[@]};
+do 
+	chcpu -e $i
+done
+
 cd /usr/src/app/tkDNN/build
 FILE=yolo4_fp16.rt
 export TKDNN_MODE=FP16
+
 if test -f "$FILE"; then
     echo "$FILE exist"
 else
